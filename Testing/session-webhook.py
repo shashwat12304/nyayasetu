69313432f64e671cd5324565d3db6c02cb5e84b0
shashwat-alyssum <shashwat.sharma@alyssum.global>
2024-06-06 17:09:19 +0530

    updated code

--- Testing/session-webhook.py
+++ Testing/session-webhook.py
@@ -39,4 +39,5 @@ def send_session_message(to, message):
     return response.json()
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
